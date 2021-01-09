89117f697195a97647c9fcf6f0ad6c97e4ffc0af
Mxkert <mikehendriks94@gmail.com>
2021-01-09 23:56:43 +0100

    New

--- index.js
+++ index.js
@@ -40,6 +40,12 @@ app.use('/posters', posters);
 //   });
 // }
 
+app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 
 function notFound(req, res, next) {
   res.status(404);
