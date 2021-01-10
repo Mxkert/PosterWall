62a2d1aa4f2bb838f60d24cee05e09938888ddcc
Mxkert <mikehendriks94@gmail.com>
2021-01-10 15:35:03 +0100

    Bug

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use('/api/posters', posters);
 
 if (process.env.NODE_ENV === 'production') {
   //set static folder
-  app.user(express.static('client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
