0fe0be80aedcdb568832d8c8d445174ea2d7dbf2
Mxkert <mikehendriks94@gmail.com>
2021-01-10 17:25:38 +0100

    L

--- index.js
+++ index.js
@@ -44,16 +44,17 @@ app.use('/api/posters', posters);
 // }
 
 // Handles any requests that don't match the ones above
-// app.use(express.static('client/build'));
-// app.get('*', (req,res) =>{
-//   res.sendFile(path.join(__dirname + '/client/build/index.html'));
-// });
-
-app.use(express.static(path.join(__dirname + '/client/build')));
-
+app.use(express.static('client/build'));
 app.get('*', (req,res) =>{
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
+
+console.log(__dirname);
+// app.use(express.static(path.join(__dirname + '/client/build')));
+
+// app.get('*', (req,res) =>{
+//   res.sendFile(path.join(__dirname + '/client/build/index.html'));
+// });
 
 function notFound(req, res, next) {
   res.status(404);
