df8c5535125995d68a9a9e351e46201b1cb1d7e7
Mxkert <mikehendriks94@gmail.com>
2021-01-10 18:39:05 +0100

    ..

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use('/api/posters', posters);
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   console.log('production');
-  app.use(express.static('client/build'));
+  app.use(express.static('../client/build'));
   app.get("/serviceWorker.js", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "src", "serviceWorker.js"));
   });
