1055104a49298b1b7026bdeeabadf8a03f281bc4
Mxkert <mikehendriks94@gmail.com>
2021-01-10 22:15:00 +0100

    Final

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(volleyball);
 app.use(cors());
 app.use(express.json());
 
-app.use('/posters', posters);
+app.use('/api/posters', posters);
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   app.use(express.static('client/build'));
