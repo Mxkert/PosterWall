7a101bd472dc7f44f08741942f2c687682074748
Mxkert <mikehendriks94@gmail.com>
2021-01-10 18:49:29 +0100

    homepage

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
+// Create Express
 const express = require('express');
 const cors = require('cors');
 const volleyball = require('volleyball');
 const path = require('path');
 const port = process.env.PORT || 5000;
 
+// mongodb+srv://Mxkert:<password>@overloader.u31ga.azure.mongodb.net/<dbname>?retryWrites=true&w=majority
+
 // MongoDB Database using Mongoose
 const mongoose = require('mongoose');
+// mongoose.connect('mongodb://127.0.0.1:27017/overloader', { useNewUrlParser: true });
 mongoose.connect('mongodb+srv://Mxkert:testtest@cluster0.jmfjg.mongodb.net/Posterwall?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
 })
 
+// Initialize express app
 const app = express();
+
+// App use
+app.use(cors());
 
 const posters = require('./routes/posters');
 app.use(volleyball);
@@ -24,7 +32,6 @@ app.use(express.json());
 app.use('/api/posters', posters);
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-  console.log('production');
   app.use(express.static('../client/build'));
   app.get("/serviceWorker.js", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "src", "serviceWorker.js"));
